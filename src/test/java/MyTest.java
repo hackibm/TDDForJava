da88b03b9740d93ab6735cb81856220ced207118
alipska <alipska.rybicka@gmail.com>
2017-02-27 14:03:53 +0100

    first commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MyTest {
+
+    @org.junit.Test
+    public void my_first_test() throws Exception {
+        assertThat(0).isEqualTo(0);
+    }
+}
